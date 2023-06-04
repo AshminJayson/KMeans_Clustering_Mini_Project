4199dd3801ab40be87f5ccb1b067ece03226caea
AnaghaAnn-gh <anaghaann2002@gmail.com>
2023-06-04 23:52:13 +0530

    Code updates in python

---
+++ MINIPROJECT/project.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,render_template
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return render_template("home.html")
+database={'helloworld@gmail.com':'123','hello@gmail.com':'345','mean@gmail.com':'average'}
+
+@app.route('/form',methods=['POST','GET'])
+def login():
+	name1=request.form['username']
+	pwd=request.form['password']
+	if name1 not in database:
+		return render_template('home.html',info='Invalid User')
+	else:
+		if database[name1]!=pwd:
+			return render_template('home.html',info='Invalid Password')
+		else:
+			return render_template('index1.html',name=name1)
+
+if __name__ == '__main__':
+    app.run()
