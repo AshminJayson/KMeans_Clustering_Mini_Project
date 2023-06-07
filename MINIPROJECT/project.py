22ca15634aefbd9ceff7ee5441a1c168d6c77d5b
AnaghaAnn-gh <anaghaann2002@gmail.com>
2023-06-07 14:59:08 +0530

    Updated code

--- MINIPROJECT/project.py
+++ MINIPROJECT/project.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template("home.html")
+  return render_template('home.html')
 database={'helloworld@gmail.com':'123','hello@gmail.com':'345','mean@gmail.com':'average'}
 
 @app.route('/index1',methods=['POST','GET'])
@@ -21,4 +21,4 @@ def login():
 			return render_template('index1.html',username=name1)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
