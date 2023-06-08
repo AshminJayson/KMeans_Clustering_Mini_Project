c6af6da52992eb50c726c74f497c10a78da5a6d0
AnaghaAnn-gh <anaghaann2002@gmail.com>
2023-06-08 13:53:32 +0530

    Updated code

--- MINIPROJECT/miniproject.py
+++ MINIPROJECT/miniproject.py
@@ -1,92 +1,39 @@
-from random import randint
-from tabulate import tabulate
+#importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
 
-def euclideandist(a, b):
-	dist = 0
-	for ind in range(len(a)):
-		dist += (a[ind] - b[ind]) ** 2
-	return (dist ** 0.5)
+pd.pandas.set_option('display.max_columns', None)
 
-def printer(dp, centroids, clusterlist, dists):
-	line = '-' * (len(centroids) + 1) * 15
-	print(line)
-	print("|{:^13}|".format("-"), end = ' ')
-	for i in centroids : 
-		print("{:^12} |".format(str(i)), end = ' ')
-	print()
-	for i in range(len(dp)):
-		print("|{:^13}|".format(str(dp[i])), end = ' ')
-		for j in range(len(centroids)):
-			print("{:^12} |".format(str(dists[i][j])), end = ' ')
-		print()
-	print(line)
-f = open('./dataset', 'r')
+from sklearn.preprocessing import StandardScaler
+import scipy.stats as stats
+from sklearn.cluster import KMeans
 
-lines = f.readlines()
-dp = []
-for line in lines:
-	dp.append(list(map(int, line.split(','))))
+#loading in the dataset
 
-num_of_dp = len(dp)
-num_of_attr = len(dp[0])
+df = pd.read_csv('../input/customer-segmentation-tutorial-in-python/Mall_Customers.csv')
+df.head()
+df.shape
+df.shape
+df.describe(include='all')
 
-k = randint(2, num_of_dp - 1)
-print("The randomly chosen number of clusters is : ", k)
+df.dtypes.value_counts() #data types
 
-centroids = []
+#checking for null values
+df.isnull().sum().sort_values(ascending=False).head()
 
-while len(centroids) != k:
-	temp = randint(0, num_of_dp - 1)
-	
-	if dp[temp] not in centroids:
-		centroids.append(dp[temp])
+#on visualizing we can confirm that there are no null values
+msno.matrix(df)
 
-print("The randomly chosen centroids are : ", centroids)
+sns.countplot(x='Gender', data=df, palette="Set3")
 
-clusterlist = [-1] * len(dp)
-check = True
-itercount = 1
-while check:
-	check = False
-	
-	dists = []
-	for pno, p in enumerate(dp):
-		closestcentroid, valclosestcentroid = -1, 100000
-		tdist = [] 
-		for i, cent in enumerate(centroids):
-			dist = euclideandist(p, cent)
-			tdist.append(round(dist, 2))
-			
-			if dist < valclosestcentroid :
-				valclosestcentroid = dist
-				closestcentroid = i
-			
-		if clusterlist[pno] != closestcentroid:
-			check = True
-			clusterlist[pno] = closestcentroid
-		dists.append(tdist)
-	
-	print("\n\nIteration : ", itercount)
-	itercount += 1
-	printer(dp, centroids, clusterlist, dists)
-	for ind in range(len(centroids)):
-		mean, count = [0] * num_of_attr, 0
-		
-		for p, clust in enumerate(clusterlist):
-			if clust == ind:
-				count += 1
-				for i in range(num_of_attr):
-					mean[i] += dp[p][i]
-					
-		for i in range(num_of_attr):
-			mean[i] /= count
-			
-		mean = [round(i, 2) for i in mean]
-		centroids[ind] = mean
-		
-		
+#lets see how gender affects to all other features.
 
+sns.pairplot(df, hue='Gender', vars=["Age", "Annual Income (k$)", "Spending Score (1-100)"],
+            palette='husl',markers=['o','D'])
 
-		
-
-
+#let's look at the correlation
+corr = df.corr()
+sns.heatmap(corr, annot=True, cmap='plasma', linewidth=0.1)
