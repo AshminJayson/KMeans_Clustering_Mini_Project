18e0c079c5917515b6edd2ddd486ffae0b24abb4
reshma-saju <86281005+reshma-saju@users.noreply.github.com>
2023-06-12 16:12:55 +0530

    made smol changes
    thank me later

--- MINIPROJECT/app.py
+++ MINIPROJECT/app.py
@@ -1,25 +1,34 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
-database={'helloworld@gmail.com':'123','hello@gmail.com':'345','mean@gmail.com':'average'}
+    return render_template('home.html')
 
-@app.route('/index1',methods=['POST','GET'])
+
+database = {'helloworld@gmail.com': '123',
+            'hello@gmail.com': '345', 'mean@gmail.com': 'average'}
+
+
+@app.route('/index1', methods=['POST', 'GET'])
 def login():
-	print(request.form)
-	name1=request.form.get('username')
-	pwd=request.form.get('password')
-	print(name1,pwd)
-	if name1 not in database:
-		return render_template('home.html',info='Invalid User')
-	else:
-		if database[name1]!=pwd:
-			return render_template('home.html',info='Invalid Password')
-		else:
-			return render_template('index1.html',username=name1)
+    if request.method == 'POST':
+        # print(request.form)
+        name1 = request.form['email']
+        pwd = request.form['password']
+        # print(name1, pwd)
+        if name1 not in database:
+            return render_template('home.html', info='Invalid User')
+        else:
+            if database[name1] != pwd:
+                return render_template('home.html', info='Invalid Password')
+            else:
+                return render_template('index1.html', username=name1)
+    else:
+        return "Method not allowed. Please use the POST method to access this route."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
