0d56bd6a9f67e3618b8764c8caf379ba3513ce7e
AnaghaAnn-gh <anaghaann2002@gmail.com>
2023-06-13 10:52:22 +0530

    Updated Backend

--- MINIPROJECT/app.py
+++ MINIPROJECT/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static/css')
 
 
 @app.route('/')
@@ -29,6 +30,15 @@ def login():
     else:
         return "Method not allowed. Please use the POST method to access this route."
 
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/file', methods=['POST','GET'])
+def upload():
+    file = request.files['file']
+    df = pd.read_csv(file)
+    return render_template('result.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
